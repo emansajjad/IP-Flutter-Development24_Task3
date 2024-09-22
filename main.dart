ba2c90015f35f85417bd591d29cb4f1f2dffa4dc
Eman <159943333+emansajjad@users.noreply.github.com>
2024-09-22 19:43:19 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'screen1.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: StartApp(),
+    );
+  }
+}
